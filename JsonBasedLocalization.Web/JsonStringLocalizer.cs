3b82f7f40f43aaca4e837039d5095fce2a7f9e3a
Ahmed AbdElaziz Shaban <65500272+zizoabdelaziz125@users.noreply.github.com>
2025-08-14 11:33:58 +0300

    Add method to get property value from Json File

---
+++ JsonBasedLocalization.Web/JsonStringLocalizer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+
+namespace JsonBasedLocalization.Web;
+
+public class JsonStringLocalizer : IStringLocalizer
+{
+    private readonly JsonSerializer _jsonSerializer = new();
+    public LocalizedString this[string name] => throw new NotImplementedException();
+
+    public LocalizedString this[string name, params object[] arguments] => throw new NotImplementedException();
+
+    public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+    {
+        throw new NotImplementedException();
+    }
+
+    private string GetValueFromJSON(string propertyName, string filePath)
+    {
+        if(string.IsNullOrEmpty(filePath) || string.IsNullOrEmpty(propertyName))
+            return string.Empty;
+
+        using FileStream fileStream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using StreamReader streamReader = new(fileStream);
+        using JsonTextReader reader = new(streamReader);
+
+        while (reader.Read())
+        {
+            if(reader.TokenType == JsonToken.PropertyName && reader.Value as string == propertyName)
+            {
+                reader.Read(); // Move to the value token
+                return _jsonSerializer.Deserialize<string>(reader) ?? string.Empty;
+            }
+        }
+
+        return string.Empty;
+
+    }
+}
